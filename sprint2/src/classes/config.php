99cc5e857dd551700af6f28613f606af0bb73515
Hmidoushvdb <137945301+TCRHamza@users.noreply.github.com>
2024-05-27 11:57:36 +0200

    biem

--- sprint2/src/classes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// auteur: studentnaam
-// functie: configuratiebestand
-
-define("DATABASE", "bas");
-define("SERVERNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-
